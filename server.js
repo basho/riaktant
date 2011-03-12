b8fc792862ba3cef9e2130af6688b2ac90704b7f
Sean Cribbs <seancribbs@gmail.com>
2011-03-12 12:46:33 -0500

    Add no.de setup.

---
+++ server.js
@@ -0,0 +1,9 @@
+// This file is the entry point for no.de
+var web = require("./lib/web/server"),
+    syslog = require("./lib/syslog-server")
+
+// Use the supplied app-server port
+web.listen(process.env.PORT || 8001)
+
+// Bind to a really high port so sudo is not needed
+syslog.bind(10514)
