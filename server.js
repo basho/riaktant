d01bf10f519bad44a6d0513054b67756585d8e17
Sean Cribbs <seancribbs@gmail.com>
2011-03-13 15:26:24 -0400

    A little code reorganization.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // This file is the entry point for no.de
 var web = require("./lib/web/server"),
-    syslog = require("./lib/syslog-server")
+    syslog = require("./lib/syslog/server")
 
 // Use the supplied app-server port
 web.listen(process.env.PORT || 8001)
